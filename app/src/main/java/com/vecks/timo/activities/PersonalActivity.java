8fe4a20c8bcc5a7b007b93180b55a97837af00c5
calo001 <calo_lrc@hotmail.com>
2017-12-11 10:11:39 -0600

    Un poco mas de DAO x.x

---
+++ app/src/main/java/com/vecks/timo/activities/PersonalActivity.java
@@ -0,0 +1,8 @@
+package com.vecks.timo.activities;
+
+/**
+ * Created by carlos on 11/12/17.
+ */
+
+public class PersonalActivity {
+}
--- app/src/main/java/com/vecks/timo/activities/RegistroTareaActivity.java
+++ app/src/main/java/com/vecks/timo/activities/RegistroTareaActivity.java
@@ -1,21 +1,20 @@
 package com.vecks.timo.activities;
 
-import android.app.Dialog;
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 import com.vecks.timo.R;
 
-public class EscolarActivity extends AppCompatActivity {
+public class RegistroTareaActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_escolar);
+        setContentView(R.layout.activity_registro_tarea);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -23,12 +22,10 @@ public class EscolarActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), RegistroTareaActivity.class);
-                startActivity(intent);
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
             }
         });
-        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
-
 
 }
